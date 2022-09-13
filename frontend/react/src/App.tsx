ac5ad1992d009d6f8c3578beda23c6627bfbab1c
cadu2inf <carlos.alves114@aluno.ce.gov.br>
2022-09-13 10:16:30 -0300

    Other components

--- frontend/react/src/App.tsx
+++ frontend/react/src/App.tsx
@@ -1,10 +1,18 @@
 import NotificationButton from "./components/NotificationButton"
+import Header from "./components/header"
+import SalesCard from "./components/SalesCard"
 
-function App() {
+function App(){
   return(
     <>
-    <h1>Olá Mundo!</h1>
-    <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   )
 }
